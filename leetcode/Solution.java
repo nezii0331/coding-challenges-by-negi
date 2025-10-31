9705861244868a0620b25feb0cc3a6c80049807a
negi <nezii0331@gmail.com>
2025-10-31 19:17:42 +0800

    ok

--- leetcode/Solution.java
+++ leetcode/Solution.java
@@ -1,3 +1,9 @@
+/*
+ * In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+ * As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+ */
+
+
 public class Solution{
     public int[] getSneakyNumbers(int [] nums){
         //create hashset() hashset to check whether repeat
