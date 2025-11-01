ce7567213027c0d9f06a7eb0df781b2c961e781a
negi <nezii0331@gmail.com>
2025-11-01 22:26:03 +0800

    DAY 3 PREPRACTICE

--- heckerrank/day02_Class_vs_Instance.java
+++ heckerrank/day02_Class_vs_Instance.java
@@ -19,7 +19,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class day02_Class_vs_Instance {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
