5fae163232c2985a9517725d512e83114223a8cb
negi <nezii0331@gmail.com>
2025-11-20 17:14:07 +0800

    binary search

---
+++ hackerrank/day13_class.java
@@ -0,0 +1,60 @@
+//hackerrank
+//Input Format
+
+// A single integer, .
+
+// Constraints
+
+// Output Format
+
+// Print a single base-10 integer that denotes the maximum number of consecutive 1's in the binary representation of n.
+
+// Sample Input 1
+
+// 5
+// Sample Output 1
+
+// 1
+// Sample Input 2
+
+// 13
+// Sample Output 2
+
+// 2
+
+import java.io.*;
+
+public class day13_class {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int count = 0;
+        int maxCount = 0;
+
+        int fackBin = 1;
+
+        // find highest power of 2
+        while (fackBin <= n) {
+            fackBin *= 2;
+        }
+        fackBin /= 2;
+
+        // scan bits
+        while (fackBin > 0) {
+            if (n >= fackBin) {
+                n -= fackBin;
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            } else {
+                count = 0;
+            }
+            fackBin /= 2;
+        }
+
+        System.out.println(maxCount);
+    }
+}
