96d3b54cb3e055742fbc018186203713277ff83e
ChenPin-Ru <nezii0331@gmail.com>
2026-03-04 14:37:28 +0800

    feat: add D4 Python Division solution and update README.md with its entry.

--- README.md
+++ README.md
@@ -87,6 +87,7 @@ Welcome to my collection of coding solutions! This repository documents my journ
 | D1 | Hello World | Basic Syntax | [Python](./hackerrank/Python/D1.py) |
 | D2 | Python If-Else | Conditionals | [Python](./hackerrank/Python/D2.py) |
 | D3 | Arithmetic Operators | Arithmetic | [Python](./hackerrank/Python/D3.py) |
+| D4 | Python: Division | Arithmetic | [Python](./hackerrank/Python/D4.py) |
 
 ### <a name="linux-shell"></a>🐧 Linux Shell
 | Day | Challenge | Topic | Solution |
---
+++ hackerrank/Python/D4.py
@@ -0,0 +1,21 @@
+# # 
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+# No rounding or formatting is necessary.
+# # 
+
+
+def printer(a, b):
+    print(int(a//b))
+    print(float(a/b)) 
+        
+if __name__ == '__main__':
+    
+    a = int(input())
+    b = int(input())
+        
+    printer(a, b)
+    
