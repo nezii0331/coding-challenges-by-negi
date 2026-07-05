80c0544be1fdd9ff27ebab8866a0e1b3ce53ce3f
negi <nezii0331@gmail.com>
2026-07-05 17:46:48 +0900

    feat: add SQL solution for HackerRank day 09 challenge to calculate difference in CITY entries

--- hackerrank/SQL/day09.sql
+++ hackerrank/SQL/day09.sql
@@ -16,4 +16,10 @@ Enter your query here.
 select distinct city from station where id % 2 = 0;
 
 
-4.
+4. Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+/*
+Enter your query here.
+*/
+
+select count(city) -  count(distinct city) from station;
